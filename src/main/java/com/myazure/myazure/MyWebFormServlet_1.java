b41467dac4bc224bf2854ec2636db7513dcc9ac6
amoolekan <146351654+amoolekan@users.noreply.github.com>
2024-04-29 00:12:13 +0100

    Update MyWebFormServlet_1.java

--- src/main/java/com/myazure/myazure/MyWebFormServlet_1.java
+++ src/main/java/com/myazure/myazure/MyWebFormServlet_1.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package com.mylab.mylab;
+//package com.mylab.mylab;
+package com.myazure.myazure;
 
 import java.io.IOException;
 /*import java.io.PrintWriter;*/
